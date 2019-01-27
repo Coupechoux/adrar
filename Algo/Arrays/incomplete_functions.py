01499cf7de2be4bde4c0572bf85eeb4a345b3d1c
Cubiix7 <pierre.coupechoux@laposte.net>
2019-01-28 00:18:49 +0100

    Arrays exercices in Python3

---
+++ Algo/Arrays/incomplete_functions.py
@@ -0,0 +1,39 @@
+﻿def swap_v1():
+	a=1
+	b=999
+	print("a vaut {} et b vaut {}".format(a,b))
+	# a vaut 1 et b vaut 999
+	
+	a=b
+	b=a
+	
+	print("a vaut {} et b vaut {}".format(a,b))
+	# a vaut 999 et b vaut 1
+	# ?
+	
+def swap_v2():
+	pass
+	
+def min_value(array):
+	pass
+	
+def max_value(array):
+	pass
+	
+def count(value,array):
+	pass
+	
+def search_v1(value,array):
+	pass
+	
+def search_v2(value,array):
+	pass
+	
+def search_in_ordered_v1(value,array):
+	pass
+	
+def two_biggest(array):
+	pass
+
+def search_in_ordered_v2(value,array,index1,index2):
+	pass
---
+++ Algo/Arrays/main.py
@@ -0,0 +1,12 @@
+﻿from incomplete_functions import *
+from mystery_functions import *
+
+swap_v1()
+# swap_v2()
+
+array_number_1 = [1,53,30,0,20,1,100,2]
+# array_number_2 = [-91,53,30,0,20,1,100,999]
+# ordered_array = [0,1,2,50,60,61,68,77,78,100,103]
+
+f4(array_number_1)
+print(array_number_1)
